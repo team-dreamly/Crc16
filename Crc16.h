404c6140d8c05aba9a0df1423a96fa16a59ebbdc
Lars Larsson <5389984+lalarsson@users.noreply.github.com>
2018-12-10 20:26:31 +0900

    Update Crc16.h

--- Crc16.h
+++ Crc16.h
@@ -29,13 +29,7 @@
 #define CRC16_H
 #define LIBRARY_VERSION_CRC16_H   "0.1.1"
 
-#if defined(ARDUINO) && ARDUINO >= 100
-  #include "Arduino.h"
-#elif defined(ARDUINO)
-  #include "WProgram.h"
-#else
-  #include <cstdint>
-#endif
+#include "Arduino.h"
 
 class Crc16 {
    private:
